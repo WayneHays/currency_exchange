3266e048878a43e406b310baf05daef81111ad89
WayneHays <waynehays1605@gmail.com>
2025-07-22 14:55:45 +0300

    feat validation rf

--- src/main/java/com/currency_exchange/util/ValidationConstants.java
+++ src/main/java/com/currency_exchange/util/ValidationConstants.java
@@ -18,7 +18,7 @@ public final class ValidationConstants {
     public static final String RATE_PATTERN = "^(?!0+([.,]0+)?$)([1-9]\\d{0,5}([.,]\\d{1,6})?|0[.,]\\d{1,6}|[.,]\\d{1,6})$";
     public static final String PAIR_PATTERN = "^([A-Za-z]{3})([A-Za-z]{3})$";
 
-    public static final String NAME_ERROR_MESSAGE = "Currency name must be 2-30 latin letters";
+    public static final String NAME_ERROR_MESSAGE = "Currency name must be 2-15 latin letters";
     public static final String CODE_ERROR_MESSAGE = "Currency code must be 3 latin letters";
     public static final String SIGN_ERROR_MESSAGE = "Currency sign must be a valid symbol (e.g. $, €...";
     public static final String RATE_ERROR_MESSAGE = "Rate must be a positive number with up to 6 digits before and after the point";
